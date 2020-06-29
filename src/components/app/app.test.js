8bec145858529af61517a9bbadaf8ba54c0c2a19
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-06-29 11:10:04 +0300

    fix: вносит правки согласно тестированию

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -123,7 +123,8 @@ const movies = [
     ratingCount: `6,2`,
     ratingLevel: `Very good`,
     votes: 300,
-  }];
+  }
+];
 
 const movieCard = {
   id: 1111,
@@ -132,8 +133,8 @@ const movieCard = {
   year: `2014`,
   starring: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`],
   director: `Wes Andreson`,
-  background: `img/the-grand-budapest-hotel-background.jpg`,
-  poster: `img/bg-the-grand-budapest-hotel.jpg`,
+  background: `img/bg-the-grand-budapest-hotel.jpg`,
+  poster: `img/the-grand-budapest-hotel-poster.jpg`,
   preview: `http://placehold.it/382x177`,
   description: [`In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege`, `Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`],
   ratingCount: `8,9`,
