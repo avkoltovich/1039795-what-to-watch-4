b64e46562dd0d2d7f21ba240898ec92ebc8ef2ef
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-06-29 09:40:50 +0300

    feat: учит тесты работать с новой структурой данных

--- src/components/small-movie-card/small-movie-card.test.js
+++ src/components/small-movie-card/small-movie-card.test.js
@@ -4,8 +4,18 @@ import SmallMovieCard from "./small-movie-card.jsx";
 
 
 const Movie = {
+  id: 5520,
   title: `Fantastic Beasts: The Crimes of Grindelwald`,
-  image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  genre: `Kids & Family`,
+  year: `2018`,
+  starring: [`Johnny Depp`, `Eddie Redmayne`, `Katherine Waterston`, `Dan Fogler`],
+  director: `David Yates`,
+  background: `https://placeimg.com/1300/512/nature`,
+  poster: `http://placehold.it/273x410`,
+  preview: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  description: [`The second installment of the "Fantastic Beasts" series featuring the adventures of Magizoologist Newt Scamander.`],
+  rating: `6,6`,
+  votes: 500
 };
 
 describe(`SmallMovieCard`, () => {
