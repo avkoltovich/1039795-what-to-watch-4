0515902dd9b5ff4d3a4fd47255185274a16cf0f0
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-06-29 11:14:08 +0300

    faet: добавляет тест для компонента moviePage

---
+++ src/components/movie-page/movie-page.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import MoviePage from "./movie-page.jsx";
+
+const Movie = {
+  id: 1111,
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  year: `2014`,
+  starring: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`],
+  director: `Wes Andreson`,
+  background: `img/the-grand-budapest-hotel-background.jpg`,
+  poster: `img/bg-the-grand-budapest-hotel.jpg`,
+  preview: `http://placehold.it/382x177`,
+  description: [`In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege`, `Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`],
+  ratingCount: `8,9`,
+  ratingLevel: `Very good`,
+  votes: 240,
+};
+
+describe(`MoviePage`, () => {
+  it(`Should render correctly`, () => {
+    const tree = renderer
+      .create(<MoviePage
+        movieCard={Movie} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
